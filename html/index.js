31c56ffdd4dd9fe11156b2aeea78c4410ddff0b0
songyubin <1970083855@qq.com>
2021-12-16 23:48:32 +0800

    原生项目后台

---
+++ html/index.js
@@ -0,0 +1,69 @@
+function getIndexId() {
+    return "index_"+Math.random().toString().substr(2,16).toString();
+}
+var indexPageVm = new Vue({
+    el: "#project-index-id",
+    data: function () {
+        return {
+            menuData:[
+                {
+                    name: "导航一",
+                    index: getIndexId(),
+                    iconName: "el-icon-location",
+                    children:[
+                        {
+                            name: "选项1",
+                            index: getIndexId(),
+                            iconName: "el-icon-location",
+                        },
+                        {
+                            name: "选项2",
+                            index: getIndexId(),
+                            iconName: "el-icon-location",
+                        },
+                        {
+                            name: "选项3",
+                            index: getIndexId(),
+                            iconName: "el-icon-location",
+                        },
+                        {
+                            name: "选项4",
+                            index: getIndexId(),
+                            iconName: "el-icon-location",
+                        }
+                    ]
+                },
+                {
+                    name: "导航二",
+                    index: getIndexId(),
+                    iconName: "el-icon-menu",
+                },
+                {
+                    name: "导航三",
+                    index: getIndexId(),
+                    iconName: "el-icon-document",
+                },
+                {
+                    name: "导航四",
+                    index: getIndexId(),
+                    iconName: "el-icon-setting",
+                },
+            ],
+            isCollapse: true
+        }
+    },
+    methods: {
+        sideCollapse: function() {
+          this.isCollapse = !this.isCollapse
+        },
+        handleOpen: function (key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose: function (key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+    mounted: function () {
+
+    }
+});
---
+++ js/md-menu.js
@@ -0,0 +1,46 @@
+var mdMenuTemplate = `
+<div>
+    <template v-for="(menuInfo, menuIndex) in menudata">
+        <template  v-if="menuInfo.children">
+            <el-submenu :index="menuInfo.index">
+                    <template  slot="title">
+                        <i :class="menuInfo.iconName"></i>
+                        <span slot="title">{{menuInfo.name}}</span>
+                    </template>
+                <md-menu v-if="menuInfo.children" :menudata="menuInfo.children" :iscollapse="iscollapse"></md-menu>
+            </el-submenu>
+        </template>
+        <template v-else>
+           <el-menu-item :index="menuInfo.index" :key="menuIndex">
+                <i :class="menuInfo.iconName"></i>
+                <span slot="title">{{menuInfo.name}}</span>
+           </el-menu-item>
+        </template>
+    </template>
+</div>
+
+`;
+Vue.component("MdMenu", {
+    name: "MdMenu",
+    template: mdMenuTemplate,
+    props: {
+        menudata: {
+            type: Array,
+            default: []
+        },
+        iscollapse: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data: function () {
+        return {}
+    },
+    watch: {},
+    methods: {
+
+    },
+    mounted: function () {
+        console.log(this.menudata);
+    }
+});
